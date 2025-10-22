9897e7295596d3cf1f1cb8780abcc8da73b76622
Viswanathan-19 <perumalvishwa70@gmail.com>
2025-10-22 11:33:26 +0530

    Exception handling Initial Commit

---
+++ ExceptionHandling/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int num1=0;
+        int num2=20;
+        int result=0;
+
+        // result=num2/num1;            Arithmetic exception
+
+        try{
+            result=num2/num1;           
+        }
+        catch(Exception e){                         //the exception is catched here,results in not stoping the program execution
+            System.out.println("Something went wrong");
+        }
+        System.out.println(result);
+
+        System.out.println("Execution doesn't stop..");
+    }
+}
