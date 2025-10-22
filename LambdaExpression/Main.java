1af1de41f6f1997367c6559abedeb4e6d8c6f6cc
Viswanathan-19 <perumalvishwa70@gmail.com>
2025-10-22 10:51:14 +0530

    LambdaExpression with Parameters passing

--- LambdaExpression/Main.java
+++ LambdaExpression/Main.java
@@ -1,14 +1,23 @@
 
 @FunctionalInterface              
 interface A{
-   void show();
+   void show(int num1,int num2);
 }
 public class Main {
     public static void main(String[] args) {
-        A obj=()->{                  //Lambda expression,the compiler knows behind the scenes
-            System.out.println("Yes it works");
+        A obj=(int n1,int n2)->{                  //Lambda expression,the compiler knows behind the scenes 
+            /*
+             new A() {                         
+            public void show(){} this is been understood by the compiler,no need to declare it explicitly.
+             */
+            System.out.println("Yes it works.. "+(n1+n2));
             
         };
-        obj.show();
+        obj.show(5,10);
     }
+
 }
+
+
+
+
