d90932d32d2b40f7fed291bfede15a2ad11f99cd
Viswanathan-19 <perumalvishwa70@gmail.com>
2025-10-22 22:28:29 +0530

    Threads Initial Commit

---
+++ Threads/MultiThreading/Main.java
@@ -0,0 +1,43 @@
+package MultiThreading;
+
+// import jdk.internal.vm.ThreadContainer;
+
+class A extends Thread{
+  
+    private String name;
+     public A(String name){
+        this.name=name;
+     }
+
+     public void run(){             //used to run the thread
+        
+        System.out.println(name+" is currently playing.."+Thread.currentThread().getName());
+
+     }
+}
+class B extends Thread{
+  
+    private String sport;
+     public B(String sport){
+        this.sport=sport;
+     }
+
+     public void run(){
+        System.out.println(sport+" is currently playing.."+Thread.currentThread().getName());
+     }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        A obj1=new A("Viswa");
+        B obj2=new B("Football");
+        B obj3=new B("Cricket");
+        B obj4=new B("Ram");
+        obj1.start();           //used to start a new thread
+        obj2.start();
+        obj3.start();
+        obj4.start();
+   
+        
+    }
+}
