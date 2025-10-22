e884bd5958dbdda6bb0f98d8301646306f550c8d
Viswanathan-19 <perumalvishwa70@gmail.com>
2025-10-22 11:00:48 +0530

    LambdaExpression with return type

--- LambdaExpression/Main.java
+++ LambdaExpression/Main.java
@@ -1,19 +1,21 @@
 
 @FunctionalInterface              
 interface A{
-   void show(int num1,int num2);
+   int show(int num1,int num2);
 }
 public class Main {
     public static void main(String[] args) {
-        A obj=(int n1,int n2)->{                  //Lambda expression,the compiler knows behind the scenes 
+        A obj=( n1, n2)->  {        //No need to declare data types,If one variable is used dont need parantheses.Lambda expression,the compiler knows behind the scenes 
             /*
              new A() {                         
             public void show(){} this is been understood by the compiler,no need to declare it explicitly.
              */
-            System.out.println("Yes it works.. "+(n1+n2));
+            System.out.println("Yes it works.. ");
+            return n1+n2;
             
-        };
-        obj.show(5,10);
+         };
+        int result =obj.show(5,10);
+        System.out.println(result);
     }
 
 }
