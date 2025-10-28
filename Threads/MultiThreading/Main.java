372d9b85e21610e8ae1bd7761a1059c7c8edc10d
Viswanathan-19 <perumalvishwa70@gmail.com>
2025-10-28 15:50:31 +0530

    Thread Priority & Sleep

--- Threads/MultiThreading/Main.java
+++ Threads/MultiThreading/Main.java
@@ -12,18 +12,30 @@ class A extends Thread{
      public void run(){             //used to run the thread
         
         System.out.println(name+" is currently playing.."+Thread.currentThread().getName());
-
+         try {
+         Thread.sleep(100);
+      } catch (InterruptedException e) {
+        
+         e.printStackTrace();
+      }
      }
 }
 class B extends Thread{
   
+    
     private String sport;
      public B(String sport){
         this.sport=sport;
      }
 
      public void run(){
-        System.out.println(sport+" is currently playing.."+Thread.currentThread().getName());
+        System.out.println(sport+" is played."+Thread.currentThread().getName());
+        try {
+         Thread.sleep(100);      //makes the thread to wait for 100millis
+      } catch (InterruptedException e) {
+        
+         e.printStackTrace();
+      }
      }
 }
 
@@ -32,12 +44,13 @@ public class Main {
         A obj1=new A("Viswa");
         B obj2=new B("Football");
         B obj3=new B("Cricket");
-        B obj4=new B("Ram");
+        A obj4=new A("Ram");
         obj1.start();           //used to start a new thread
         obj2.start();
         obj3.start();
         obj4.start();
    
-        
+        obj1.setPriority(Thread.MAX_PRIORITY);           // can set the priority
+        System.out.println(obj1.getPriority());
     }
 }
